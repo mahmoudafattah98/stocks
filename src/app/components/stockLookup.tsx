61a7a1dcc5fb85ff022293df16651ee494c2863e
mahmoudafattah98 <128329870+mahmoudafattah98@users.noreply.github.com>
2024-08-23 00:29:56 +0300

    added types, fixed useState type declarations, fixed fetchStocks, added resetStocks, added renderStocks, fixed onSearch, fixed some styling

--- src/app/components/stockLookup.tsx
+++ src/app/components/stockLookup.tsx
@@ -1,16 +1,41 @@
-import { Input, Grid } from "antd";
+import { Input, Grid, Spin, Empty, Button, Avatar } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 import { Col, Row } from "antd";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Card } from "antd";
+import React from "react";
 
 const { Search } = Input;
 
-type stock = {};
+type Stock = {
+  ticker: string;
+  name: string;
+  market: string;
+  locale: string;
+};
+type SearchResult = {
+  count?: number;
+  request_id: string;
+  results: Array<Stock>;
+  status: string;
+};
 
 export default function StockLookup() {
-  const [stocks, setStocks]: [stock, any] = useState([]);
+  const [stocks, setStocks]: [Stock[], Dispatch<SetStateAction<Stock[]>>] =
+    useState([{ ticker: "", name: "", market: "", locale: "" }]);
   const [isLoading, setIsLoading] = useState(false);
   const [ticker, setTicker] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult]: [
+    SearchResult,
+    Dispatch<SetStateAction<SearchResult>>
+  ] = useState<SearchResult>({
+    count: 0,
+    request_id: "",
+    results: [],
+    status: "",
+  });
 
   const today = new Date().toISOString().split("T")[0];
   const yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000)
@@ -30,12 +55,10 @@ export default function StockLookup() {
         throw new Error(`API error: ${response.status} ${response.statusText}`);
       }
 
-      // Parse the JSON if the response is in JSON format
       const data = await response.json();
       console.log("Parsed JSON:", data);
-
-      // Process the data as needed
-      setStocks([data]);
+      setSearchResult(data);
+      setStocks(data.results);
     } catch (error) {
       console.error("Error fetching stock data:", error);
     } finally {
@@ -43,8 +66,39 @@ export default function StockLookup() {
     }
   }
 
+  function resetStocks() {
+    setStocks([]);
+    setSearchTerm("");
+    setSearchResult({ count: 0, request_id: "", results: [], status: "" });
+  }
   function renderStocks() {
-    return <></>;
+    if (isLoading) {
+      return <Spin />;
+    }
+
+    if (stocks.length === 0) {
+      return (
+        <Empty>
+          <Button onClick={() => resetStocks}>Reset Search</Button>
+        </Empty>
+      );
+    }
+
+    return (
+      <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
+        {stocks.map((stock) => (
+          <Col span={6} key={uuidv4()}>
+            <Card hoverable bordered={false} style={{ flexDirection: "row" }}>
+              <Avatar>{stock.ticker.substring(0, 2)}</Avatar>
+              <div style={{ flexDirection: "column" }}>
+                <p style={{ textDecoration: "bold" }}>{stock.ticker}</p>
+                <p>{stock.name}</p>
+              </div>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    );
   }
 
   return (
@@ -54,12 +108,13 @@ export default function StockLookup() {
           <Search
             placeholder="Search for stocks by entering ticker e.g. AAPL, MCFT"
             size="large"
-            onSearch={fetchStocks}
+            onSearch={(searchTerm) => fetchStocks(searchTerm)}
+            style={{ marginBottom: "20px" }}
           />
         </Col>
       </Row>
 
-      <Row>{renderStocks()}</Row>
+      {renderStocks()}
     </>
   );
 }
