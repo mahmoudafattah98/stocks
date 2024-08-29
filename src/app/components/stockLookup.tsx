d96eeb084f66d138be8588beb5533e6bc6505176
mahmoudafattah98 <128329870+mahmoudafattah98@users.noreply.github.com>
2024-08-29 11:43:36 +0300

    Added pagination using useInfiniteQuery

--- src/app/components/stockLookup.tsx
+++ src/app/components/stockLookup.tsx
@@ -4,6 +4,7 @@ import { Col, Row } from "antd";
 import { useEffect, useState } from "react";
 import { Card } from "antd";
 import { v4 as uuidv4 } from "uuid";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const { Search } = Input;
 const apiKey = "viPbJp10BxQKnao_CgbjW3rpmf26RZbt";
@@ -23,69 +24,61 @@ type SearchResult = {
 };
 
 export default function StockLookup() {
-  const [stocks, setStocks] = useState<Stock[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [searchResult, setSearchResult] = useState<SearchResult>({
-    count: 0,
-    request_id: "",
-    results: [],
-    status: "",
-  });
+  const [ticker, setTicker] = useState<string>("");
   const [rateLimited, setRateLimited] = useState(false);
 
-  useEffect(() => {
-    fetchStocks("");
-    logWhenBottomReached();
-  }, []);
-
-  async function fetchStocks(ticker: string) {
-    setIsLoading(true);
-    setRateLimited(false);
-
-    try {
-      const response = await fetch(
-        `https://api.polygon.io/v3/reference/tickers?search=${ticker}&active=true&apiKey=${apiKey}`
-      );
-
-      if (response.status === 429) {
-        setRateLimited(true);
-        return;
-      }
-
-      if (!response.ok) {
-        throw new Error(`API error: ${response.status} ${response.statusText}`);
-      }
-
-      const data = await response.json();
-      setSearchResult(data);
-      setStocks(data.results);
-    } catch (error) {
-      console.error("Error fetching stock data:", error);
-    } finally {
-      setIsLoading(false);
+  const fetchStocks = async ({ pageParam = "" }) => {
+    const response = await fetch(
+      `https://api.polygon.io/v3/reference/tickers?search=${ticker}&active=true&limit=12&cursor=${pageParam}&apiKey=${apiKey}`
+    );
+    if (response.status === 429) {
+      setRateLimited(true);
+      return null;
     }
-  }
+    const data = await response.json();
+    return data;
+  };
 
-  function resetStocks() {
-    setStocks([]);
-    setSearchResult({ count: 0, request_id: "", results: [], status: "" });
-    setRateLimited(false);
-  }
-
-  function logWhenBottomReached() {
-    window.addEventListener("scroll", () => {
-      const scrollTop = window.scrollY;
-      const windowHeight = window.innerHeight;
-      const documentHeight = document.documentElement.scrollHeight;
-
-      console.log("Scroll event detected");
-      if (windowHeight + scrollTop >= documentHeight - 1) {
-        console.log("Bottom of the page reached");
-      }
+  const { data, fetchNextPage, hasNextPage, isLoading, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: [ticker],
+      queryFn: fetchStocks,
+      getNextPageParam: (lastPage) => {
+        if (lastPage === null) {
+          return undefined;
+        }
+        return lastPage.next_url?.split("cursor=")[1];
+      },
+      initialPageParam: "",
+      enabled: !!ticker,
     });
-  }
 
-  function renderStocks() {
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight - 2
+      ) {
+        if (hasNextPage && !isFetchingNextPage) {
+          fetchNextPage();
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+
+  const handleSearch = (searchTerm: string) => {
+    setTicker(searchTerm);
+  };
+
+  const resetStocks = () => {
+    setTicker("");
+    setRateLimited(false);
+  };
+
+  const renderStocks = () => {
     if (isLoading) {
       return (
         <div
@@ -104,35 +97,35 @@ export default function StockLookup() {
       );
     }
 
-    if (rateLimited) {
-      return (
-        <div
-          style={{
-            height: "65vh",
-            alignItems: "center",
-            justifyContent: "center",
-            display: "flex",
-            flexDirection: "column",
-            textAlign: "center",
-          }}
-        >
-          <CloseCircleFilled
-            style={{ fontSize: "50px", color: "red", marginBottom: "20px" }}
-          />
-          <p style={{ fontWeight: "bold", fontSize: "24px" }}>
-            You have reached your maximum number of requests.
-          </p>
-          <p style={{ fontSize: "14px", marginBottom: "20px" }}>
-            Please wait at least 1 minute before retrying.
-          </p>
-          <Button type="primary" onClick={resetStocks}>
-            Reset Search
-          </Button>
-        </div>
-      );
-    }
+    // if (rateLimited) {
+    //   return (
+    //     <div
+    //       style={{
+    //         height: "65vh",
+    //         alignItems: "center",
+    //         justifyContent: "center",
+    //         display: "flex",
+    //         flexDirection: "column",
+    //         textAlign: "center",
+    //       }}
+    //     >
+    //       <CloseCircleFilled
+    //         style={{ fontSize: "50px", color: "red", marginBottom: "20px" }}
+    //       />
+    //       <p style={{ fontWeight: "bold", fontSize: "24px" }}>
+    //         You have reached your maximum number of requests.
+    //       </p>
+    //       <p style={{ fontSize: "14px", marginBottom: "20px" }}>
+    //         Please wait at least 1 minute before retrying.
+    //       </p>
+    //       <Button type="primary" onClick={resetStocks}>
+    //         Reset Search
+    //       </Button>
+    //     </div>
+    //   );
+    // }
 
-    if (stocks.length === 0) {
+    if (!data?.pages?.[0]?.results.length) {
       return (
         <div
           style={{
@@ -150,32 +143,41 @@ export default function StockLookup() {
     }
 
     return (
-      <Row gutter={[16, 16]}>
-        {stocks.map((stock) => (
-          <Col key={uuidv4()} xs={24} sm={12} md={8} lg={6}>
-            <Card
-              title={
-                <Avatar
-                  size={52}
-                  style={{ backgroundColor: "#fde3cf", color: "#f56a00" }}
+      <>
+        <Row gutter={[16, 16]}>
+          {data.pages.map((page) =>
+            page?.results?.map((stock: Stock) => (
+              <Col key={uuidv4()} xs={24} sm={12} md={8} lg={6}>
+                <Card
+                  title={
+                    <Avatar
+                      size={52}
+                      style={{ backgroundColor: "#fde3cf", color: "#f56a00" }}
+                    >
+                      {stock.ticker.substring(0, 2)}
+                    </Avatar>
+                  }
+                  hoverable
+                  bordered={false}
+                  style={{ display: "flex", flexDirection: "row" }}
                 >
-                  {stock.ticker.substring(0, 2)}
-                </Avatar>
-              }
-              hoverable
-              bordered={false}
-              style={{ display: "flex", flexDirection: "row" }}
-            >
-              <div style={{ flexDirection: "column" }}>
-                <p style={{ fontWeight: "bold" }}>{stock.ticker}</p>
-                <p>{stock.name.substring(0, 37)}</p>
-              </div>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+                  <div style={{ flexDirection: "column" }}>
+                    <p style={{ fontWeight: "bold" }}>{stock.ticker}</p>
+                    <p>{stock.name.substring(0, 37)}</p>
+                  </div>
+                </Card>
+              </Col>
+            ))
+          )}
+        </Row>
+        {isFetchingNextPage && (
+          <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <Spin size="large" />
+          </div>
+        )}
+      </>
     );
-  }
+  };
 
   return (
     <>
@@ -185,7 +187,7 @@ export default function StockLookup() {
             id="search"
             placeholder="Search for stocks by entering ticker e.g. AAPL, MCFT"
             size="large"
-            onSearch={(searchTerm) => fetchStocks(searchTerm)}
+            onSearch={handleSearch}
             style={{ marginBottom: "40px" }}
           />
         </Col>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,17 @@
 // import styles from "./page.module.css";
 import { Roboto } from "next/font/google";
 import Initial from "./components/initial";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function Home() {
   return (
     // <main className={styles.main}>
     <main>
-      <Initial />
+      <QueryClientProvider client={queryClient}>
+        <Initial />
+      </QueryClientProvider>
     </main>
   );
 }
